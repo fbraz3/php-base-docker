9ed05318e4b9cf1c018510d75808d39c0223b1b8
Felipe Keller Braz <fbraz3@gmail.com>
2025-04-24 20:14:47 -0300

    feat(gh-actions): change project to serve cli

--- assets/scripts/docker-entrypoint.sh
+++ assets/scripts/docker-entrypoint.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-$(which chmod) 700 /etc/monit/monitrc
-
-FILE_TO_WATCH="/dev/null"
-
-# CLEAR TMP FILES
-/root/autoclean.sh
-
-# ADD CRON
-CRONFILE="/cronfile.final"
-SYSTEMCRON="/cronfile.system"
-USERCRON="/cronfile"
-
-echo > $CRONFILE
-if [ -f "$SYSTEMCRON" ]; then
-	cat $SYSTEMCRON >> $CRONFILE
-fi
-if [ -f "$USERCRON" ]; then
-	cat $USERCRON >> $CRONFILE
-fi
-/usr/bin/crontab $CRONFILE
-
 # DECLARE/SET VARIABLES
 PHPVERSION=`cat /PHP_VERSION 2>/dev/null`
 if [ -z "$PHPVERSION" ]; then
@@ -31,22 +10,15 @@ if [ -z "$PHPVERSION" ]; then
     PHPVERSION='7.3'
 fi
 
-# PUPULATE TEMPLATES
+if [ -z "$ENTRYPOINT_COMMAND" ]; then
+    ENTRYPOINT_COMMAND='/usr/bin/php'
+fi
+
 if [ -d "/entrypoints" ]; then
 	for file in /entrypoints/*.sh; do
      [ -f "$file" ] || continue
-	 echo "> Running: $file"
      source $file
  done
 fi
 
-# START SERVICES
-echo "> Starting Services"
-/usr/sbin/service cron restart
-sleep 1
-/usr/sbin/service monit start
-
-# KEEP CONTAINER ALIVE
-if [ ! -z "$FILE_TO_WATCH" ]; then
-	/usr/bin/tail -f $FILE_TO_WATCH
-fi
+exec $ENTRYPOINT_COMMAND "$@"
